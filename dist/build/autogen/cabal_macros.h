e759ee5b1d204ae5df826d77067837738ddfae79
Siddharth Trehan <trehansiddharth@gmail.com>
2014-12-29 00:50:31 -0800

    Bugfix: cabal dependencies

--- dist/build/autogen/cabal_macros.h
+++ dist/build/autogen/cabal_macros.h
@@ -7,17 +7,24 @@
   (major1) == 2 && (major2) <  1 || \
   (major1) == 2 && (major2) == 1 && (minor) <= 0)
 
-/* package base-4.6.0.1 */
-#define VERSION_base "4.6.0.1"
+/* package automaton-0.1.0.0 */
+#define VERSION_automaton "0.1.0.0"
+#define MIN_VERSION_automaton(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  1 || \
+  (major1) == 0 && (major2) == 1 && (minor) <= 0)
+
+/* package base-4.5.0.0 */
+#define VERSION_base "4.5.0.0"
 #define MIN_VERSION_base(major1,major2,minor) (\
   (major1) <  4 || \
-  (major1) == 4 && (major2) <  6 || \
-  (major1) == 4 && (major2) == 6 && (minor) <= 0)
+  (major1) == 4 && (major2) <  5 || \
+  (major1) == 4 && (major2) == 5 && (minor) <= 0)
 
-/* package containers-0.5.0.0 */
-#define VERSION_containers "0.5.0.0"
+/* package containers-0.5.5.1 */
+#define VERSION_containers "0.5.5.1"
 #define MIN_VERSION_containers(major1,major2,minor) (\
   (major1) <  0 || \
   (major1) == 0 && (major2) <  5 || \
-  (major1) == 0 && (major2) == 5 && (minor) <= 0)
+  (major1) == 0 && (major2) == 5 && (minor) <= 5)
 
